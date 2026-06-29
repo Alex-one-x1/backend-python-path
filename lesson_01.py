545af5bd58312c6492061791293ca8d89d57b1d9
AlekseyDavydovskiy <tesla.one.x1@gmail.com>
2026-06-29 04:17:43 +0300

    Занятие 1: первая программа — расчёт мощности

---
+++ lesson_01.py
@@ -0,0 +1,9 @@
+# вход:
+voltage = float(input("Введите значение напряжения (В): "))
+current = float(input("Введите значение тока (А): "))
+
+# обработка:
+power = voltage * current
+
+# выход:
+print(f"Результат расчета мощности: {power} Вт")
